efea9d1b21a0d516273df50760dc333a4424b151
Darío Rumbo <dario.rumbo@gmail.com>
2022-03-29 00:25:27 -0300

    Final

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import AboutPage from './pages/AboutPage';
+import ContactPage from './pages/ContactPage';
+import Error404 from './pages/Error404';
+import HomePage from './pages/HomePage';
+import UserPage from './pages/UserPage';
+import UsersPage from './pages/UsersPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+        <Navbar /> 
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/users" element={<UsersPage />} />
+          <Route path="/users/:id" element={<UserPage />} />
+          <Route path="/about" element={<AboutPage />}>
+            <Route path="contact" element={<ContactPage />} />
+          </Route>
+          <Route path="*" element={<Error404 />} />
+        </Routes>
+      </Router>
   );
 }
 
